c804cf4ee81866510798f23a75733d6f92a18b1e
Remone Teleporta <remoneteleporta@gmail.com>
2025-12-01 23:18:30 +0530

    blog and footer set

---
+++ js/bloglist.js
@@ -0,0 +1,50 @@
+export const bloglist = [
+{
+title: "My new journey as a bootcamp student",
+img: "",
+desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+date: "November 25, 2025"
+},
+{
+title: "Blog One",
+img: "",
+desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+date: "November 25, 2025"
+},
+{
+title: "Blog Two",
+img: "",
+desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+date: "November 26, 2025"
+},
+{
+title: "Blog Three",
+img: "",
+desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+date: "November 27, 2025"
+},
+{
+title: "Blog Four",
+img: "",
+desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+date: "November 28, 2025"
+},
+{
+title: "Blog Five",
+img: "",
+desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+date: "November 29, 2025"
+},
+{
+title: "Blog Six",
+img: "",
+desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+date: "November 30, 2025"
+},
+{
+title: "Blog Seven",
+img: "",
+desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+date: "December 01, 2025"
+}
+]

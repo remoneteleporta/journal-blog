d962eafee5ffaf04d8dd90c582f708477f8995a0
Remone Teleporta <remoneteleporta@gmail.com>
2025-12-04 16:40:45 +0530

    recents posts mobile css complete

--- js/blog.js
+++ js/blog.js
@@ -2,10 +2,12 @@ import {bloglist} from "/js/bloglist.js"
 const recentPost = document.getElementById("recent-posts")
 
 recentPost.innerHTML = bloglist.slice(0, 3).map(blogs =>{
-return `<div class="recent-post-carousel"> 
+return `<div class="recent-post-carousel">
+<a href="${blogs.url}">
 <img src="${blogs.img}">
 <time>${blogs.date}</time>
 <h4>${blogs.title}</h4>
 <p>${blogs.desc}</p>
+</a>
 </div>`
 }).join("")
--- js/bloglist.js
+++ js/bloglist.js
@@ -1,49 +1,58 @@
 export const bloglist = [
-{
-title: "My new journey as a bootcamp student",
-img: "/blog/bootcamp-student-journey/journey-bootcamp-cs-student.jpg",
-desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-date: "November 25, 2025"
-},
-{
-title: "Blog One",
-img: "/blog/blog-1/cloud-tech-ai-monopoly-risks.jpg",
-desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-date: "November 25, 2025"
-},
-{
-title: "Blog Two",
-img: "/blog/blog-2/tech-solutions-and-burnout.jpg",
-desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-date: "November 26, 2025"
-},
-{
-title: "Blog Three",
-img: "/blog/blog-3/ai-model-content-internet-degradation-collapse.jpg",
-desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-date: "November 27, 2025"
-},
-{
-title: "Blog Four",
-img: "/blog/blog-4/stock-markets-must-be-abolished.jpg",
-desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-date: "November 28, 2025"
-},
-{
-title: "Blog Five",
-img: "/blog/blog-5/corporate-politics.jpg",
-desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-date: "November 29, 2025"
-},
-{
-title: "Blog Six",
-img: "/blog/blog-6/tech-prejudice-imposter-syndrome.jpg",
-desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-date: "November 30, 2025"
-},
-{
-title: "Blog Seven",
-img: "/blog/blog-7/impossible-artificial-general-intelligence.jpg",
-desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-date: "December 01, 2025"
-}]
+  {
+    "title": "Blog Seven",
+    "img": "/blog/blog-7/impossible-artificial-general-intelligence.jpg",
+    "desc": "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    "url": "/blog/blog-7/",
+    "date": "December 01, 2025"
+  },
+  {
+    "title": "Blog Six",
+    "img": "/blog/blog-6/tech-prejudice-imposter-syndrome.jpg",
+    "desc": "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    "url": "/blog/blog-6/",
+    "date": "November 30, 2025"
+  },
+  {
+    "title": "Blog Five",
+    "img": "/blog/blog-5/corporate-politics.jpg",
+    "desc": "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    "url": "/blog/blog-5/",
+    "date": "November 29, 2025"
+  },
+  {
+    "title": "Blog Four",
+    "img": "/blog/blog-4/stock-markets-must-be-abolished.jpg",
+    "desc": "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    "url": "/blog/blog-4/",
+    "date": "November 28, 2025"
+  },
+  {
+    "title": "Blog Three",
+    "img": "/blog/blog-3/ai-model-content-internet-degradation-collapse.jpg",
+    "desc": "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    "url": "/blog/blog-3/",
+    "date": "November 27, 2025"
+  },
+  {
+    "title": "Blog Two",
+    "img": "/blog/blog-2/tech-solutions-and-burnout.jpg",
+    "desc": "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    "url": "/blog/blog-2/",
+    "date": "November 26, 2025"
+  },
+  {
+    "title": "Blog One",
+    "img": "/blog/blog-1/cloud-tech-ai-monopoly-risks.jpg",
+    "desc": "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    "url": "/blog/blog-1/",
+    "date": "November 25, 2025"
+  },
+  {
+    "title": "My new journey as a bootcamp student",
+    "img": "/blog/bootcamp-student-journey/journey-bootcamp-cs-student.jpg",
+    "desc": "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    "url": "/blog/bootcamp-student-journey/",
+    "date": "November 25, 2025"
+  }
+]

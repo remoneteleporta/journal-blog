c94b7d3041233bb60d9dd4407382e7252abb6341
Remone Teleporta <remoneteleporta@gmail.com>
2025-12-20 14:54:49 +0530

    about page responsive nav added

--- js/about.js
+++ js/about.js
@@ -1,6 +1,8 @@
 import { bloglist } from "/js/bloglist.js"
 const recentPost = document.getElementById("recent-posts")
-
+const hamMenuBtn = document.getElementById("menu-btn")
+const navLinks = document.getElementById("nav-links")
+const closeMenuBtn = document.getElementById("close-menu-btn")
 
 recentPost.innerHTML = bloglist.slice(bloglist.length - 3).reverse().map(blogs =>{
 return `<div class="recent-post-carousel">
@@ -12,3 +14,11 @@ return `<div class="recent-post-carousel">
 </a>
 </div>`
 }).join("")
+
+hamMenuBtn.addEventListener("click", ()=>{
+     navLinks.style.display = "flex";
+})
+
+closeMenuBtn.addEventListener("click",()=>{
+    navLinks.style.display = "none";
+})

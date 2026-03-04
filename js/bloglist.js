5c3d72913fbfcd42ecb59fdd0de9e535c4fb8df0
Remone Teleporta <remoneteleporta@gmail.com>
2026-03-04 19:54:29 +0530

    stock markets blog changes

--- js/bloglist.js
+++ js/bloglist.js
@@ -29,7 +29,7 @@ export const bloglist = [
   },
   {
     "title": "Blog Four",
-    "img": "blog/abolish-stock-markets/stock-markets-must-be-abolished.jpg",
+    "img": "/blog/abolish-stock-markets/stock-markets-must-be-abolished.jpg",
     "desc": "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
     "url": "/blog/abolish-stock-markets/",
     "date": "November 28, 2025"
